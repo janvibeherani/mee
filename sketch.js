eee1e56113a8e0dfb49decc637847805cffe91e2
janvibeherani <janvi.beherani.sod23@anu.edu.in>
2024-07-19 12:37:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ function setup() {
 //create a surface to draw on
  createCanvas(600, 600);
   
-  background(200,123,0);
+  background(29, 110, 161);
   strokeWeight(10);
   stroke('white');
   //point(300,300);
